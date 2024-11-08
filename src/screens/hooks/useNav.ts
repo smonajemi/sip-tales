19050af551a570fa0c8c495b7535dd34669030cd
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2024-11-08 02:46:56 -0500

    refactored

--- src/screens/hooks/useNav.ts
+++ src/screens/hooks/useNav.ts
@@ -2,11 +2,20 @@
 import { useNavigate } from 'react-router-dom';
 
 const useNav = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+
+  // const redirectTo = (path: string) => {
+  //   navigate(path);
+  // };
 
   const redirectTo = (path: string) => {
-    navigate(path);
+    const clickEvent = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+    });
+    document.querySelector(`a[href='${path}']`)?.dispatchEvent(clickEvent);
   };
+  
 
   return {
     redirectTo,
